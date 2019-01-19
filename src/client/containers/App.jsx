1f6bd77def9539bddada83ca4dc5b4012036fc01
Chang Yea Moon <changyeamoon@gmail.com>
2019-01-18 19:02:34 -0800

    Created a HostController to handle a post and get route using middleware for events table. Added Body parser and url encoded.

--- src/client/containers/App.jsx
+++ src/client/containers/App.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import './app.css';
+import './styles/app.css';
 
 export default class App extends Component {
   state = { username: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    // fetch('/api/getUsername')
+    //   .then(res => res.json())
+    //   .then(user => this.setState({ username: user.username }));
   }
 
   render() {
