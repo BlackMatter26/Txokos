273a23f95ef41204fc47d5e337705edca3bfe1aa
trischoe <tristans7@gmail.com>
2019-01-18 21:49:53 -0800

    added methods to hostController : addFoodItem, addInvite, getUserId, added routes to index: add_food, add_invite,

--- src/server/controllers/HostController.js
+++ src/server/controllers/HostController.js
@@ -3,11 +3,11 @@ const db = require('../models/database.js');
 const hostController = {
   createEvent(req, res, next) {
     db.query('INSERT INTO event SET ?', {
-      host_id: req.params.host_id,
-      time: req.body.event_time,
+      host_id: req.params.hostId,
+      time: req.body.eventTime,
       location: req.body.location,
-      max_attend: req.body.max_attend,
-      event_name: req.body.event_name,
+      max_attend: req.body.maxAttend,
+      event_name: req.body.eventName,
     }, (err, results) => {
       if (err) throw err;
       res.locals.eventId = results.insertId;
@@ -24,7 +24,48 @@ const hostController = {
       next();
     });
   },
-};
 
+  getEvents(req, res, next) {
+    db.query('SELECT * from event WHERE host_id = ?', [req.body.hostId], function (err, results){
+      if(err) throw err;
+      res.locals.results = results;
+      db.end();
+      next();
+    });
+  },
+
+  addFoodItem(req, res, next) {
+    db.query('INSERT INTO event_food SET ?', {
+      event_id: req.params.eventId,
+      food_name: req.body.foodName,
+    }, function (err, results){
+      if(err) throw err;
+      res.locals.eventFoodId = results.insertId;
+      db.end();
+      next();
+    });
+  },
+
+  addInvite(req, res, next) {
+    db.query('INSERT INTO event_user SET ?', {
+      user_id: req.body.userId,
+      event_id: req.params.eventId,
+    }, function (err, results){
+      if(err) throw err;
+      res.locals.userEventsId = results.insertID;
+      db.end();
+      next();
+    });
+  },
+
+  getUserId(req, res, next) {
+    db.query('SELECT user_id from USERS WHERE email = ?', [req.params.email], function(err, results){
+      if(err) throw err;
+      res.locals.userId = results;
+      db.end();
+      next();
+    });
+  }
+};
 
 module.exports = hostController;
--- src/server/index.js
+++ src/server/index.js
@@ -19,5 +19,13 @@ app.post('/new_event/:host_id', HostController.createEvent, (req, res) => {
   res.status(200).json(res.locals.eventId);
 });
 
+//add a food item for an event
+app.post('/add_food/:eventId', HostController.addFoodItem, (req, res) => {
+  res.status(200).json(res.locals.eventFoodId);
+});
 
+//invite a user to an event
+app.post('/add_invite/:eventId', HostController.getUserId, HostController.addInvite, (req, res) => {
+  res.status(200).json(res.locals.userEventsId);
+});
 app.listen(8080, () => console.log('Listening on port 8080!'));
